4ca4488574b0892c610699267a6543fbbd6d67d2
Stoorx <antiproton2013@yandex.ru>
2019-10-28 21:14:51 +0300

    GH pages release

---
+++ precache-manifest.7270d8ae2f57a1e716891c60cf4bb1c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74996c448576edb2a5a4a92692b95f70",
+    "url": "/Web2lab2/index.html"
+  },
+  {
+    "revision": "eb47ad56e7b67c777c50",
+    "url": "/Web2lab2/static/css/main.2818e4ec.chunk.css"
+  },
+  {
+    "revision": "7881df4b8c4224cfef24",
+    "url": "/Web2lab2/static/js/2.a435c9b0.chunk.js"
+  },
+  {
+    "revision": "eb47ad56e7b67c777c50",
+    "url": "/Web2lab2/static/js/main.594bad8f.chunk.js"
+  },
+  {
+    "revision": "5c217ec126b8505bfab0",
+    "url": "/Web2lab2/static/js/runtime-main.98ab5bc4.js"
+  }
+]);
